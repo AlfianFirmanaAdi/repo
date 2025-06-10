74da3108848160b3c0b43fd360a670abae3cd990
AlfianFirmanaAdi <138551723+AlfianFirmanaAdi@users.noreply.github.com>
2025-06-10 15:34:44 +0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -340,9 +340,9 @@ else:
     # --- Tombol Mode Hapus dan Edit ---
     # Menggunakan kolom dengan rasio untuk menempatkan tombol di kanan
     # [3] untuk kolom kosong yang mendorong, [1] untuk Pilih Hapus, [1] untuk Edit Caption
-    col_gallery_actions = st.columns([3, 1, 1])
+    col_gallery_actions = st.columns([3, 1, 0])
     
-    with col_gallery_actions[1]: # Tempatkan Pilih Hapus di kolom kedua
+    with col_gallery_actions[0]: # Tempatkan Pilih Hapus di kolom kedua
         # Tombol Toggle Hapus
         if st.session_state.delete_mode:
             if st.button("🚫 Batal Hapus", key="cancel_delete_mode"):
